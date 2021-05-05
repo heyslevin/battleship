a34bac7a393a1352d048455edc9538e7a11d7324
memellaguno <memellaguno@gmail.com>
2021-05-05 08:58:02 -0500

    Starting Player Factory

--- src/factories/playerFactory.js
+++ src/factories/playerFactory.js
@@ -1,4 +1,29 @@
 const Player = () => {
+  const aiPlay = function aiActionWhenPlaying() {
+    const playedCoordinates = [];
+
+    /*
+        Process:
+        
+        # Player creates 5 ships
+        # Player places ships
+        # Player has a score, of Sunken Ships
+        # Player picks coordinate to attack
+        
+        # AI
+          - BEGINNER AI
+            - Computer selects random spaces
+            - Generate 2 random numbers, each between 0 and 9
+            - If coordinate has been already hit, try another until not hit
+          - ADVANCED AI
+            - If there is a hit, computer tries adjacent tiles
+            - If there is a second hit, and a "direction" is defined, computer continues line on one side.
+            - If no hit on that side, computer tries other side
+
+
+        */
+  };
+
   return true;
 };
 
